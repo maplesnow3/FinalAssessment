4051247174dc601bcba981b29d777e1252314af0
Yifan <inaufrist@gmail.com>
2021-10-21 06:23:36 +1100

    Added basic layout but without function

--- app/src/main/java/comp5216/finalAssessment/UserLogin.java
+++ app/src/main/java/comp5216/finalAssessment/UserLogin.java
@@ -2,17 +2,21 @@ package comp5216.finalAssessment;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserLogin extends AppCompatActivity {
 
     Button logIn, signUp;
+    EditText userName, passWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userlogin);
-
+        userName = findViewById(R.id.editTextuserName);
+        passWord = findViewById(R.id.editTextuserPassword);
     }
 }
