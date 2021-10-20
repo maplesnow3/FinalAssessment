0ba0d8b08dee9fa53a2efa6b5b6513290e709b2c
Yifan <inaufrist@gmail.com>
2021-10-20 23:09:45 +1100

    Added basic layout but without function

--- app/src/main/java/comp5216/finalAssessment/UserLogin.java
+++ app/src/main/java/comp5216/finalAssessment/UserLogin.java
@@ -1,4 +1,18 @@
 package comp5216.finalAssessment;
 
-public class UserLogin {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserLogin extends AppCompatActivity {
+
+    Button logIn, signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userlogin);
+
+    }
 }
