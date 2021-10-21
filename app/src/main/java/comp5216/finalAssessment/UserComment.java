15c5c80afd0d554f0903b4e915ded613ac4aab4b
nogipen <choipenpen@gmail.com>
2021-10-22 02:31:02 +0800

    layout update on comment

---
+++ app/src/main/java/comp5216/finalAssessment/UserComment.java
@@ -0,0 +1,12 @@
+package comp5216.finalAssessment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserComment extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_comment);
+    }
+}
